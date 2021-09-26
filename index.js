2c3669112d0cc48ad4fc525fd2e659b7070b18c5
allistermugaisi <allistermugaisi@gmail.com>
2021-09-26 20:00:15 +0300

    configured environment PORT

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ mongoose
 	.then(() => console.log('MongoDB connected successfully!'))
 	.catch((error) => console.log(error.message));
 
-const PORT = process.env.port || 5000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () =>
 	console.log(`Server running successfully on port ${PORT}.`)
