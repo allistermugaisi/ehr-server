2797b13bb079a407e05b3da51105bf8eecc5d376
allistermugaisi <allistermugaisi@gmail.com>
2021-11-30 00:38:12 +0300

    changed server port

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ app.use(express.json()); // used to parse JSON bodies
 app.use(express.urlencoded({ limit: '30mb', extended: true })); // parse URL-encoded bodies
 app.use(
 	cors({
-		origin: 'http://localhost:3080',
+		origin: 'http://localhost:3000',
 		optionsSuccessStatus: 200, // For legacy browser support
 	})
 );
